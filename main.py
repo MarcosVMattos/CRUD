f212ca64f13deea05cb7a29ff924544ac477794c
Marcos Mattos <marcos98mattos@gmail.com>
2026-07-15 20:04:42 -0300

    refactor: reorganize project structure for SQLAlchemy

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
 
+from database import Base, engine
+from models import Task
 from routers.tasks import router
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(router=router)
-
-DATABASE_URL = "sqlite:///tasks.db"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
-
-class Task(Base):
-    __tablename__ = "tasks"
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    description = Column(String)
-
-Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
